fabe74c87cbd43a9cfbc3282c295deafb4bec7f6
supasuma <supasuma@gmail.com>
2016-09-15 16:56:00 +0100

    a bunch of css

--- jasmine-standalone-2.5.0/src/interface.js
+++ jasmine-standalone-2.5.0/src/interface.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
 
+
    var thermostat = new Thermostat();
 
     $("#temperature").text(thermostat.temperature);
 
-    $("body").css({backgroundColor:thermostat.energyChecker()});
+    $("").css({backgroundColor:thermostat.energyChecker()});
 
     $("#temperature-up").click(function() {
         thermostat.increaseTemperature();
@@ -20,17 +21,32 @@ $(document).ready(function() {
     $("#reset").click(function() {
       thermostat.resetTemp();
       $("#temperature").text(thermostat.temperature);
-      $("body").css({backgroundColor:thermostat.energyChecker()});
+      $("main").css({backgroundColor:thermostat.energyChecker()});
     });
 
     $("#power-save-on").click(function() {
       thermostat.powerSaveOn();
+      $(this).toggleClass("active");
+      $('#power-save-off').toggleClass("active");
       $('#power-save-message').text(thermostat.isPowerSaveOn());
     });
 
     $("#power-save-off").click(function() {
       thermostat.powerSaveOff();
+      $('#power-save-on').toggleClass("active");
+      $(this).toggleClass("active");
       $('#power-save-message').text(thermostat.isPowerSaveOn());
     });
+
+    $("#enter-city").submit(function(event) {
+      var input = $('#enter-city :input');
+      var city = (input[0].value);
+      event.preventDefault();
+      $.getJSON("http://api.wunderground.com/api/e0d820621ded8ea4/conditions/q/GB/"+city+".json", function(data) {
+        $("#current-temp").text(data.current_observation.temp_c);
+      });
+    });
+
+
 
 });
